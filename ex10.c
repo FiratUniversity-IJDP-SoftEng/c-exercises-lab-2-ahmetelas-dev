3eacceffe817991b3db5ddeb3bc98e9dae2d34c6
ahmetelas-dev <elasahmet22@gmail.com>
2025-12-06 21:57:31 +0300

    Update ex10.c

--- ex10.c
+++ ex10.c
@@ -1,1 +1,26 @@
+// AHMET FARUK ELAS
 
+#include <stdio.h>
+
+int main(void) 
+{
+    int i, j;
+
+    printf("   X |   1   2   3   4   5   6   7   8   9  10\n");
+    printf("--------------------------------------------------\n");
+
+    for (i = 1; i <= 10; i++) 
+    {
+        
+        printf("%d |", i);
+
+        for (j = 1; j <= 10; j++) 
+        {
+            printf("%4d", i * j);
+        }
+        
+        printf("\n"); 
+    }
+
+    return 0;
+}
