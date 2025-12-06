556b07a0353aba914b2ec23153ef5a832083451f
ahmetelas-dev <elasahmet22@gmail.com>
2025-12-06 22:45:30 +0300

    Update ex13.c

--- ex13.c
+++ ex13.c
@@ -1,1 +1,29 @@
+//AHMET FARUK ELAS
 
+#include <stdio.h>
+
+int fac(int n)
+{
+    
+    if (n <= 1)
+    {
+        return 1;
+    }
+    
+    else
+    {
+        return n * fac(n - 1);
+    }
+}
+
+int main(void)
+{
+    int a;
+
+    printf("Enter an integer to calculate Factorial: ");
+    scanf("%d", &a);
+
+    printf("%d! = %d\n", a, fac(a)); 
+
+    return 0;
+}
