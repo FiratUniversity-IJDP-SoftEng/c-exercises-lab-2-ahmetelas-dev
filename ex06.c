9f2c7649d5144cef76d5fd994983be1aba81e0bd
ahmetelas-dev <elasahmet22@gmail.com>
2025-12-06 15:18:57 +0300

    Update ex06.c

--- ex06.c
+++ ex06.c
@@ -1,1 +1,40 @@
+// AHMET FARUK ELAS
 
+#include <stdio.h>
+
+int main(void) 
+{
+    
+    int number[30];
+    int index = 0; 
+
+    printf("number\n");
+
+    for (int i = 100; i >= 50; i--) 
+    {
+        
+        if (i % 3 == 0) 
+        {
+            
+            number[index] = i;
+        
+            index++;
+            
+            if (index >= 30) 
+            {
+                break; 
+            }
+        }
+    }
+    
+    int sum = index;
+
+    printf("\nElements within the arrays\n");
+    
+    for (int j = 0; j < sum; j++) {
+        
+        printf("Dizi[%d]: %d\n", j, number[j]);
+    }
+
+    return 0;
+}
