885e2784749cb439f286876c0b636cbcdb8592e5
ahmetelas-dev <elasahmet22@gmail.com>
2025-12-07 00:02:41 +0300

    Update ex14.c

--- ex14.c
+++ ex14.c
@@ -1,1 +1,22 @@
+//AHMET FARUK ELAS
 
+#include <stdio.h>
+
+int fibonacci(int n) {
+    
+    if (n == 0) return 0;
+    if (n == 1) return 1;
+
+    return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+int main() {
+    int n, i;
+
+    printf("Enter the position to stop at: ");
+    scanf("%d", &n);
+
+    for (i = 0; i < n; i++) {
+        printf("%d ", fibonacci(i));
+    }
+}
