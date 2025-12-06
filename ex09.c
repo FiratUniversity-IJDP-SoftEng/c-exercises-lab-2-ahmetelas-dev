4980558a08f7765bd43f6801250d16118512b3fa
ahmetelas-dev <elasahmet22@gmail.com>
2025-12-06 21:40:11 +0300

    Update ex09.c

--- ex09.c
+++ ex09.c
@@ -1,1 +1,42 @@
+// AHMET FARUK ELAS
 
+#include <stdio.h>
+
+int main() 
+{
+    int num;
+    int count = 0;
+    int sum = 0;
+    float average = 0.0;
+
+    printf("enter numbers (enter -1 to stop):\n");
+
+    while (1) 
+    {
+        scanf("%d", &num);
+
+        if (num == -1) 
+        {
+            break;
+        }
+
+        sum += num;
+        count++;
+    }
+
+    if (count > 0) 
+    {
+        average = (float)sum / count; 
+        
+        printf("count: %d\n", count);
+        printf("sum: %d\n", sum);
+        printf("average: %.2f\n", average);
+    } 
+
+    else 
+    {
+        printf("pelase enter valid numbers\n");
+    }
+
+    return 0;
+}
