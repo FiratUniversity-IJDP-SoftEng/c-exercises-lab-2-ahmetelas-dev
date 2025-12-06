6af002c403dfadeacd4c481637ded100db51481e
ahmetelas-dev <elasahmet22@gmail.com>
2025-12-06 21:16:27 +0300

    Update ex08.c

--- ex08.c
+++ ex08.c
@@ -1,1 +1,24 @@
+//AGMET FARUK ELAS
 
+#include <stdio.h>
+
+int main(void)
+{
+    int number[10];
+    int a;
+    int final = 0;
+
+    printf("Please enter 10 integer\n");
+    for(a = 0; a < 10; a++)
+    {
+        scanf("%d", &number[a]);
+    }
+    
+    for(int sum = 0; sum <= 9; sum++)
+    {
+        final += number[sum];
+    }
+    double avarage = (double)final / a;
+    printf("their sum is %d \n", final);
+    printf("their avarage is %.2lf", avarage);
+}
