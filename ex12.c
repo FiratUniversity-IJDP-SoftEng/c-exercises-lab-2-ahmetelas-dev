8c35926b8c27ae8f0030139a188af53ffb2febcc
ahmetelas-dev <elasahmet22@gmail.com>
2025-12-06 22:31:59 +0300

    Update ex12.c

--- ex12.c
+++ ex12.c
@@ -1,1 +1,17 @@
+//AHMET FARUK ELAS
 
+#include <stdio.h>
+
+int main(void)
+{
+    int a;
+    int final = 1;
+    printf("enter a intager to calculate Factorial: ");
+    scanf("%d", &a);
+    for(int i = 1; i <= a; i++)
+    {
+        final *= i;
+    }
+
+    printf("%d! = %d", a, final);
+}
