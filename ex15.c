3c2cf3187c7240eb6da3cdf5cf8eff5ded0a3603
ahmetelas-dev <elasahmet22@gmail.com>
2025-12-07 00:29:14 +0300

    Update ex15.c

--- ex15.c
+++ ex15.c
@@ -1,1 +1,51 @@
+//AHMET FARUK ELAS
 
+#include <stdio.h>
+
+int main(void) 
+{
+    int n = 5;
+    int i, j, k, s;
+
+    for (i = 1; i <= n; i++) 
+    {
+        
+        for (s = 1; s <= n - i; s++) 
+        {
+            printf(" ");
+        }
+
+        for (j = 1; j <= i; j++) 
+        {
+            printf("%d", j);
+        }
+
+        for (k = i - 1; k >= 1; k--) 
+        {
+            printf("%d", k);
+        }
+
+        printf("\n"); 
+    }
+
+    for (i = n - 1; i >= 1; i--) 
+    {
+        
+        for (s = 1; s <= n - i; s++) 
+        {
+            printf(" ");
+        }
+
+        for (j = 1; j <= i; j++) 
+        {
+            printf("%d", j);
+        }
+
+        for (k = i - 1; k >= 1; k--) 
+        {
+            printf("%d", k);
+        }
+
+        printf("\n");
+    }
+}
