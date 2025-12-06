fdf60574d810798e2768bddfbc01c9e4e9ab8485
ahmetelas-dev <elasahmet22@gmail.com>
2025-12-06 15:54:47 +0300

    Update ex07.c

--- ex07.c
+++ ex07.c
@@ -1,1 +1,27 @@
+#include <stdio.h>
 
+int first(int i)
+{
+    return i = 5 * i;
+}
+int second(int i)
+{
+    return i = 99 - (10 * i) + 10;
+}
+int third(int i)
+{
+    return i;
+}
+
+int main(void)
+{
+    int i;
+
+    
+    printf("Please enter your number:");
+    scanf("%d", &i);
+    
+    printf("pillar 1 \tpilar 2\t\tpillar 3\n");
+
+    printf("%d\t\t%d\t\t%d", first(i), second(i), third(i));
+}
